0904c2af256cb14976ad1aafac0f86462c40a2a0
malo21st <44739330+malo21st@users.noreply.github.com>
2022-12-21 21:14:30 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -25,11 +25,11 @@ def answer(question):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=question,
-        temperature=0,
+        temperature=0.9,
         max_tokens=300,
-        top_p=1.0,
+        top_p=1,
         frequency_penalty=0.0,
-        presence_penalty=0.0,
+        presence_penalty=0.6,
     )
     return response.choices[0].text.split("\n")[-1]
 
