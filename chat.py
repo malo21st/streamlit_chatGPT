762ca2909eebec1e485a5cb42542a37f0282180e
malo21st <44739330+malo21st@users.noreply.github.com>
2022-12-23 04:00:31 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -7,8 +7,8 @@ openai.api_key = st.secrets['api_key']
 
 st.set_page_config(
     page_title = "ChatGPT chatbot",
-#     page_icon = "🤖"
-    page_icon = st.image(".favicon.png")
+    page_icon = "🤖"
+#     page_icon = st.image("favicon.png")
 )
 
 if 'generated' not in st.session_state:
